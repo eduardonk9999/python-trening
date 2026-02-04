41e8496e11d4d31cac8fc8c1053961386a913439
Eduardo Silva <thiago.novak7@gmail.com>
2026-02-04 08:48:13 -0300

    scripe0

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # python-trening
 
-
-
-## proejto
---
+++ basico/logica.py
@@ -0,0 +1,10 @@
+Logica de programacao é o estudo das linguagens das maquinas
+
+Algoritmos é um conjunto de passos finitos e organizados, que quando executados,
+resolvem determinados problemas.
+
+Variaveis e Constantes => sao elementos que em um programa
+manipula. Uma variavel é um espaco na memoria do computador
+para armazenar um tipo de dado determinado.
+Embora uma variável possa assumir diferentes valores, ela só
+pode assumir um valor por vez.
